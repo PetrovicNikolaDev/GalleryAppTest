44a86e19a3988f96b7d67464b78ffa1053fa777e
Nikola Petrovic <nikola.sms90@hotmail.com>
2020-10-18 07:03:48 +0200

    Added Test for Login and Create Gallerie

---
+++ GalleryAppTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalleryAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create reference for the browser
+            IWebDriver driver = new ChromeDriver();
+
+            //Navigate to GalleryApp Login
+            driver.Navigate().GoToUrl("https://gallery-app.vivifyideas.com/login");
+
+            driver.FindElement(By.Id("email")).SendKeys("nik@gallery.com");
+            driver.FindElement(By.Id("password")).SendKeys("12345678");
+            driver.FindElement(By.TagName("button")).Click();
+        }
+
+        
+    }
+}
